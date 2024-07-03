744852f11be8616281bb3a2e7290b3562b57c9ce
Aniketdhama <aniketdhama951@gmail.com>
2024-07-03 12:02:57 +0530

    Initial commit

--- client/src/Component/Login.js
+++ client/src/Component/Login.js
@@ -81,7 +81,7 @@ const Login = ({ isUserAuthenticated }) => {
     const [account, toggleAccount] = useState('login');
     const navigate = useNavigate();
     const { setAccount } = useContext(DataContext);
-    const imageURL = 'https://images.unsplash.com/photo-1688382139135-950a4e886ead?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80';
+   // const imageURL = 'https://images.unsplash.com/photo-1688382139135-950a4e886ead?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80';
     useEffect(() => {
         showError(false);
     }, [login])
